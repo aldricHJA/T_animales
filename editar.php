24e6d24196f224384dd3fcdced1dda397c4a2697
aldricHJA <aldrichmills@gmail.com>
2022-11-14 16:56:23 -0600

    entrar

--- editar.php
+++ editar.php
@@ -1,5 +1,6 @@
 <?php
 include "./conexion.php";
+include "./header.php";
 $conexion = conexion();
 $id = $_GET['idp'];
 $sql = "SELECT * FROM t_animales
@@ -10,18 +11,51 @@ $item = mysqli_fetch_array($respuestas);
 
 
 <html>
-    <h2>Actualizar</h2>
+    <body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+
+            <div class="card">
+  <div class="card-header">
+  </div>
+  <div class="card-body">
+    <center><h5 class="card-title">Actualizar</h5></center>
     <form action="./backend/actualizar.php" method="post">
         <input type="text" name="id" value="<?php echo $id;?>" hidden>
         <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre" value="<?php echo $item['nombre'] ?>">
+        <input type="text" name="nombre" id="nombre" class="form-control" required value="<?php echo $item['nombre'] ?>">
         <br>
         <label for="region">Region</label>
-        <input type="text" name="region" id="region" value="<?php echo $item['region']?>">
+        <input type="text" name="region" id="region" class="form-control" required value="<?php echo $item['region']?>">
         <br>
         <label for="comida">Comida</label>
-        <input type="text" name="comida" id="comida" value="<?php echo $item['comida']?>">
+        <input type="text" name="comida" id="comida" class="form-control" required value="<?php echo $item['comida']?>">
         <br>
-        <button>Actualizar</button>
+        <center>
+        <a href="#" class="btn btn-warning">Actualizar</a>
+        <a href="./index.php" class="btn btn-primary">Regresar</a>
+        </center>
     </form>
+ </div>
+    </div>
+        </div>
+            </div>
+                </div>
+
+
+
+
+
+
+
+
+        
+    
+    <h2>Actualizar</h2>
+   
+    </body>
 </html>
+
+<?php include "./footer.php"?>
